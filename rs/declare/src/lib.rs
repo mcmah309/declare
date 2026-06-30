4c31a156bc3b087ad5fd51929cd964c19b584bf3
mcmah309 <dillonmcmahon79@gmail.com>
2026-06-30 10:52:45 +0000

    Add project scaffolding

---
+++ rs/declare/src/lib.rs
@@ -0,0 +1,2 @@
+// Enables feature flag documentation on things in docs.rs https://github.com/rust-lang/rust/issues/43781 http://doc.rust-lang.org/rustdoc/unstable-features.html#doccfg-and-docauto_cfg
+#![cfg_attr(docsrs, feature(doc_cfg))]
